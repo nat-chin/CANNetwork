4375172df50d311249038930ccb535f734809ac8
nat-chin <138374958+nat-chin@users.noreply.github.com>
2024-04-25 09:06:02 +0700

    Separate each src build path

--- src/unoNode.cpp
+++ src/unoNode.cpp
@@ -2,9 +2,24 @@
 #include <SPI.h>
 #include <Wire.h>
 
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+
 // put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
 
 // put function definitions here:
-int myFunction2(int x, int y) {
+int myFunction(int x, int y) {
   return x + y;
 }
